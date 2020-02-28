618893bd38d1c4563475c89f622391ec4fb5d4d9
Gaodong Liu <1120496618@qq.com>
2020-02-28 23:47:46 +0800

    update

---
+++ learn_python_the_hard_way/ex40.py
@@ -0,0 +1,18 @@
+class Song(object):
+
+    def __init__(self, lyrics):
+        self.lyrics = lyrics
+
+    def sing_me_a_song(self):
+        for line in self.lyrics:
+            print(line)
+
+happy_baby = Song(["Happy birthday to you",
+"I don't want to get sued", "So I'll stop right there"])
+
+bulls_on_parade = Song(["They rally around tha family",
+"With pockets full of shells"])
+
+happy_baby.sing_me_a_song()
+
+bulls_on_parade.sing_me_a_song()
